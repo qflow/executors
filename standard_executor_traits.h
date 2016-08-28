50797e5c1b0717f2d50a42fe6f2b7f97bab7a685
mfojtak <mfojtak@seznam.cz>
2016-08-28 21:43:11 +0200

    update

--- standard_executor_traits.h
+++ standard_executor_traits.h
@@ -2,7 +2,7 @@
 #define STANDARD_EXECUTOR_TRAITS_H
 #include "executor_traits.h"
 
-template<class Function, typename Executor>
+template<class Function, class Executor>
 auto async_execute(Executor& ex, Function&& f)
 {
     return executor_traits<Executor>::async_execute(ex, f);
@@ -13,9 +13,13 @@ auto then_execute(Executor& ex, Function&& f, Future&& fut)
     return executor_traits<Executor>::then_execute(ex, f, fut);
 }
 template<class executor_type, class... Futures>
-static auto when_all(executor_type& ex, Futures&&... futures)
+auto when_all(executor_type& ex, Futures&&... futures)
 {
     return executor_traits<executor_type>::when_all(ex, futures...);
 }
-
+template<class executor_type, class Function, class Range>
+auto for_each(executor_type& ex, Function&& func, Range& range)
+{
+    return executor_traits<executor_type>::for_each(ex, func, range);
+}
 #endif
